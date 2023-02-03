bee78e183fdcdd8c6269d5afd8010a136dc63538
Heil Park <heilpark@me.com>
2023-02-03 21:33:24 +0000

    fixed layout and cleaned up text

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $("#search-button").on("click", function(event) {
     event.preventDefault();
 
     var search = $("#search-input").val().trim();
-    
+
     if (search == "") {
         return;
     }
@@ -24,6 +24,8 @@ $("#search-button").on("click", function(event) {
 });
 
 function currentInfo({ lat, lon }) {
+    $("#today").empty();
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" 
     + lon + "&units=metric&appid=b163cdbf260a24669a52af24040a91f0";
 
@@ -31,12 +33,12 @@ function currentInfo({ lat, lon }) {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        $("#today").text(`${response.name} (${moment().format("DD/MM/YYYY")})`);
+        var title = $("<h4>").addClass("h5").text(`${response.name} (${moment().format("DD/MM/YYYY")})`);
         var icon = $("<img>").attr("src", `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
-        var temp = $("<p>").text(`${response.main.temp} °C`);
-        var wind = $("<p>").text(`${response.wind.speed} KPH`);
-        var humid = $("<p>").text(`${response.main.humidity}%`);
-        $("#today").append(icon, temp, wind, humid);
+        var temp = $("<p>").text(`Temp: ${response.main.temp} °C`);
+        var wind = $("<p>").text(`Wind: ${response.wind.speed} KPH`);
+        var humid = $("<p>").text(`Humidity: ${response.main.humidity}%`);
+        $("#today").append(title, icon, temp, wind, humid);
     });
 
     forecastInfo(lat, lon);
@@ -47,13 +49,12 @@ function displayForecast(response) {
 
     for (let i =0; i < response.length;i++) {
         var card = $("<div>").addClass("card");
-
-        card.text(response[i].dt_txt);
+        var title = $("<h5>").addClass("h6").text(response[i].dt_txt);
         var icon = $("<img>").attr("src", `http://openweathermap.org/img/w/${response[i].weather[0].icon}.png`);
-        var temp = $("<p>").text(`${response[i].main.temp} °C`);
-        var wind = $("<p>").text(`${response[i].wind.speed} KPH`);
-        var humid = $("<p>").text(`${response[i].main.humidity}%`);
-        card.append(icon, temp, wind, humid);
+        var temp = $("<p>").text(`Temp: ${response[i].main.temp} °C`);
+        var wind = $("<p>").text(`Wind: ${response[i].wind.speed} KPH`);
+        var humid = $("<p>").text(`Humidity: ${response[i].main.humidity}%`);
+        card.append(title, icon, temp, wind, humid);
 
         $("#forecast").append(card);
     }
@@ -71,7 +72,9 @@ function forecastInfo(lat, lon) {
 
         for (let i =0; i< response.list.length; i++) {
             if (response.list[i].dt_txt.includes("12:00:00")) {
-                days.push(response.list[i]);
+                var day = response.list[i];
+                day.dt_txt = day.dt_txt.replace(' 12:00:00','');
+                days.push(day);
             }
         }
 
@@ -90,7 +93,7 @@ function renderHistory() {
       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var a = $("<button>");
       // Adding a class of movie-btn to our button
-      a.addClass("hist-btn");
+      a.addClass("hist-btn rounded");
       // Adding a data-attribute
       a.attr("data-name", locations[i]);
       // Providing the initial button text
